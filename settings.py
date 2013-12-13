fd15077858610ebb5346233e15db2ffc8db853c9
Jing <jing@thinkpad.(none)>
2013-12-12 21:31:12 -0500

    setting

--- settings.py
+++ settings.py
@@ -214,11 +214,11 @@ STATIC_URL = "/static/"
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT,'project_static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(PROJECT_ROOT,'project_static'),
+#)
 
 if not hasattr(globals(), 'SECRET_KEY'):
     SECRET_FILE = os.path.join(PROJECT_ROOT, 'secret.txt')
